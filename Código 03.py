5472851b84e7c308f368d3be09bbb66ffec6693f
lincoln1606 <161850475+lincoln1606@users.noreply.github.com>
2025-04-10 12:24:00 -0300

    Create Código 03.py

---
+++ Código 03.py
@@ -0,0 +1,15 @@
+letra = input('Digite uma letra: ')
+
+
+if letra == 'a':
+    print('Sua letra é uma vogal')
+elif letra == 'e':
+    print('Sua letra é uma vogal')
+elif letra == 'i':
+    print('Sua letra é uma vogal')
+elif letra == 'o':
+    print('Sua letra é uma vogal')
+elif letra == 'u':
+    print('Sua letra é uma vogal')
+else:
+    print('Sua letra é uma consoante')
