e8676b3e0dd164a7d2e0a0bdfd0e91cf8562562a
lincoln1606 <161850475+lincoln1606@users.noreply.github.com>
2025-04-10 12:23:31 -0300

    Create Código 02.py

---
+++ Código 02.py
@@ -0,0 +1,10 @@
+numero = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
+
+
+if numero > numero2:
+    print('O primeiro numero é maior.')
+elif numero2 > numero:
+    print('O segundo numero é maior.')
+elif numero == numero2:
+    print("Os numeros são iguais.")
